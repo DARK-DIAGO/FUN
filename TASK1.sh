50bdb3b0c1eecbbae993ce84812fde08f9b986a1
DARK-DIAGO <70196362+DARK-DIAGO@users.noreply.github.com>
2020-08-25 13:19:50 +0530

    TASK1.sh
    SEND THIS TO YOUR FRIENDS 🐱

---
+++ TASK1.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+echo "HELLO AND WELCOME THIS IS BASH SCRIPT."
+echo "HELLO FRIENDS HOPE YOU ARE FIT AND FINE BY GRACE OF GOD"
+echo "SO CONTINUE IT"
+echo "ENTER YOUR NAME TO CONTINUE :"
+read a1
+echo "ENTER YOU AGE :"
+read a2
+echo "YOURS PRIVACY IS HERE"
+sleep 3
+echo "YOUR NAME IS    :$a1"
+echo "YOUR AGE IS     :$a2"
+sleep 2
+printf "THIS WAS MY FIRST SCRIPT"
+sleep 3
+echo
+printf "HOPE YOU ENJOYED IT"
+sleep 2
+clear
+echo "HAVE A NICE DAY BRO BECAUSE YOU IS MY FIRST USER"
+echo "its over now"
+sleep 3
+clear
